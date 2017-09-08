98180f52a623f8c207b843d000da3092edc43bd3
ucer <185429135@qq.com>
2017-09-08 21:39:10 +0800

    init project

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('real_name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->enum('is_admin', ['yes','no'])->default('no')->index()->comment('是否为后台管理员：no 否，yes 是');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
